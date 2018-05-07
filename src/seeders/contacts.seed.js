9fe899d2710736e24e83164b99c29c86ed4a1c7b
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-05-08 00:39:45 +0300

    refactoring, link routes update middleware bug fix

--- src/seeders/contacts.seed.js
+++ src/seeders/contacts.seed.js
@@ -11,17 +11,25 @@ const seedContacts = async () => {
     const data = [{
         name: 'Sofia',
         value: '10 Aleksandar Malinov boulevard, Sofia',
+        email: 'cometenomer1@comet.com',
+        phone: '+359 11 12 34',
         isPrimary: true,
     }, {
         name: 'Plovidiv',
         value: 'Plovdiv Center, Plovdiv',
+        email: 'cometenomer1@comet.com',
+        phone: '+359 11 12 34',
         isPrimary: true,
     }, {
         name: 'Bansko',
         value: '9 Angel Kanchev 2770 Noviyat Grad, Bansko',
+        email: 'cometenomer1@comet.com',
+        phone: '+359 11 12 34',
     }, {
         name: 'Knezha',
         value: '53 Dimitar Butanski, Knezha',
+        email: 'cometenomer1@comet.com',
+        phone: '+359 11 12 34',
     }];
 
     return await Contact.insertMany(data);
